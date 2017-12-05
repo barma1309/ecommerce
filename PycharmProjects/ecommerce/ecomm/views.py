c3074a4983ca77fb44a93c2a92ea918b8d905723
barma1309 <romich1309@gmail.com>
2017-12-05 23:11:52 +0200

    add html template #2

--- PycharmProjects/ecommerce/ecomm/views.py
+++ PycharmProjects/ecommerce/ecomm/views.py
@@ -6,7 +6,25 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def home_page(request):
-    return render(request, "home_page.html", {})
+    context={
+        "title":"home page",
+        "content":"welcome to the home page"
+    }
+    return render(request, "home_page.html", context)
+
+def about_page(request):
+    context = {
+        "title": "about page",
+        "content":"welcome to the about page"
+    }
+    return render(request, "home_page.html", context)
+
+def contact_page(request):
+    context = {
+        "title": "contact page",
+        "content":"welcome to the contact page"
+    }
+    return render(request, "home_page.html", context)
 
 
 def home_page_old(request):
--- PycharmProjects/ecommerce/ecommerce/urls.py
+++ PycharmProjects/ecommerce/ecommerce/urls.py
@@ -15,11 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from ecomm.views import home_page
+from ecomm.views import home_page, about_page, contact_page
 
 
 
 urlpatterns = [
     url(r'^$', home_page),
+    url(r'^about/$', about_page),
+    url(r'^contact/$', contact_page),
     url(r'^admin/', admin.site.urls),
 ]
