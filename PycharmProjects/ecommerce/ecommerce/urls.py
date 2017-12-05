2583add9f5b9a96cb2e219b40b882d7f1a7d1bdd
barma1309 <romich1309@gmail.com>
2017-12-05 20:00:31 +0200

    Hello world

--- PycharmProjects/ecommerce/ecommerce/urls.py
+++ PycharmProjects/ecommerce/ecommerce/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from ecomm.views import home_page
+
 
 
 urlpatterns = [
+    url(r'^$', home_page),
     url(r'^admin/', admin.site.urls),
 ]
